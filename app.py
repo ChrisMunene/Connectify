7ac5a70f76f71f86b3b97b1c5dee82a3c1f06c75
Ernest Ochieng' Omondi <eomondi@college.harvard.edu>
2017-12-07 02:27:12 +0000

    Changed * again

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
+        rows = db.execute("SELECT username, hash, userid FROM users WHERE username = :username",
                           username=request.form.get("username"))
 
         # Ensure username exists and password is correct
